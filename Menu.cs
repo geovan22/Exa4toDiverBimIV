832707861e795817b8e31d97a5fa3c8b70fb3b28
geovan22 <43213313+geovan22@users.noreply.github.com>
2021-10-07 21:44:35 -0600

    Add files via upload

---
+++ Menu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExaBimIV
+{
+    class Menu
+    {
+        public int MenuPecera()
+        {
+            Console.WriteLine("1.- Ingresar Datos");
+            Console.WriteLine("2.- Litros");
+            Console.WriteLine("3.- Galones");
+            Console.WriteLine("4.- Salir");
+            return int.Parse();
+        }
+    }
+}
---
+++ Pecera.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExaBimIV
+{
+    class Pecera
+    {
+        private double galones = 0;
+        private double litros = 0;
+
+        public void CalculoLitros(double largo, double ancho, double alto)
+        {
+            litros = largo * ancho * largo;
+        }
+
+        public double VerLitros()
+        {
+            return litros;
+        }
+
+        public double CalculoGalones()
+        { 
+        
+        }
+    }
+}
